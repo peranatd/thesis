405fb4160c31cd68eda9e9f3f255096669872a4b
Sujin Lee <pasdesujin@gmail.com>
2016-08-05 18:00:59 -0400

    (fix) Escape the single quote

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
     <h1> Home </h1>
     <img src="http://nordicapis.com/wp-content/uploads/20-emotion-recognition-apis-nordic-apis.png"  alt="main" />
     <p>Speak Mirror</p>
-    <p>Friends don't let friends talk to themselves in front of a mirror. Yuan - Your friend and CEO</p>
+    <p>Friends don\'t let friends talk to themselves in front of a mirror. Yuan - Your friend and CEO</p>
     <button>Start Now</button>
   </div>
   )
