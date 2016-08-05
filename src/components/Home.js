a62561de18ebd1a81e48f48128cc552900855d30
Sujin Lee <pasdesujin@gmail.com>
2016-08-05 18:18:55 -0400

    (style) Change words on the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Home = () => (
   <div>
     <h1> Home </h1>
-    <img src="http://nordicapis.com/wp-content/uploads/20-emotion-recognition-apis-nordic-apis.png"  alt="main" />
-    <p>Speak Mirror</p>
-    <p>Friends don\'t let friends talk to themselves in front of a mirror. Yuan - Your friend and CEO</p>
+    <blockquote>
+    <h3>Speak Mirror</h3>
+    <p>Our app will give you honest feedback on how others perceive the way you speak, as you are speaking. We utilise powerful APIs to bring you the most cutting-edge automated analysis of the most important traits of a great communicator
+    </p>
+    </blockquote>
     <button>Start Now</button>
   </div>
   )
