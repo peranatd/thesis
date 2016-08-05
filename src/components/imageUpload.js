5459a9049842a30416e2396f3fbe31755e7d24a5
Yuan Liu <liuyuanneu@gmail.com>
2016-08-05 18:39:42 -0400

    (refactor) Use react-dropzone module instead of plain javascript

--- src/components/imageUpload.js
+++ src/components/imageUpload.js
@@ -1,67 +1,113 @@
 import React from 'react';
 import $ from 'jquery';
+import Dropzone from 'react-dropzone';
 
 class ImageUpload extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
-      file: '',
-      imagePreviewUrl: ''
-    };
+      files: []
+    }
   }
+  onDrop(files){
+    this.setState({
+      files: files
+    })
+    let data = new FormData();
+    $.each(files, function(key, value){
+        data.append(key, value);
+    });
 
-  handleSubmit(e) {
-    e.preventDefault();
-    //console.log('handle uploading-', this.state.file);
-    //console.log('imageurl', this.state.imagePreviewUrl);
     $.ajax({
       url:"/api/image",
       type:"POST",
-      data: this.state.file,
+      data: data,
+      cache: false,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
       success: function(results) {
         //maybe redirect to result page
+        console.log(results)
       },
       error: function(err) {
         console.log(err);
       }
     });
   }
-
-  handleImageChange(e) {
-    e.preventDefault();
-
-    let reader = new FileReader();
-    let file = e.target.files[0];
-    reader.onloadend = () => {
-      this.setState({
-        file: file,
-        imagePreviewUrl: reader.result
-      });
-    }
-
-    reader.readAsDataURL(file)
-  }
-  render() {
-    let {imagePreviewUrl} = this.state;
-    let $imagePreview = null;
-    if (imagePreviewUrl) {
-      $imagePreview = (<img src={imagePreviewUrl} />);
-    } else {
-      $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
-    }
-
+  render(){
     return (
-      <div className="previewComponent">
-        <form onSubmit={(e)=>this.handleSubmit(e)}>
-          <input className="fileInput" type="file" onChange={(e)=>this.handleImageChange(e)} />
-          <button className="submitButton" type="submit" onClick={(e)=>this.handleSubmit(e)}>Upload Image</button>
-        </form>
-        <div className="imgPreview">
-          {$imagePreview}
+      <div>
+        <Dropzone onDrop={this.onDrop.bind(this)}>
+          <div>Try dropping some files here, or click to select files to upload.</div>
+        </Dropzone>
+        <div>
+        {this.state.files.map((file,index) => <img key={index}src={file.preview} width='200' alt={file.name}/> )}
         </div>
       </div>
     )
   }
 }
+// class ImageUpload extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       file: '',
+//       imagePreviewUrl: ''
+//     };
+//   }
 
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     //console.log('handle uploading-', this.state.file);
+//     //console.log('imageurl', this.state.imagePreviewUrl);
+//     $.ajax({
+//       url:"/api/image",
+//       type:"POST",
+//       data: this.state.file,
+//       success: function(results) {
+//         //maybe redirect to result page
+//       },
+//       error: function(err) {
+//         console.log(err);
+//       }
+//     });
+//   }
+
+//   handleImageChange(e) {
+//     e.preventDefault();
+
+//     let reader = new FileReader();
+//     let file = e.target.files[0];
+//     reader.onloadend = () => {
+//       this.setState({
+//         file: file,
+//         imagePreviewUrl: reader.result
+//       });
+//     }
+
+//     reader.readAsDataURL(file)
+//   }
+//   render() {
+//     let {imagePreviewUrl} = this.state;
+//     let $imagePreview = null;
+//     if (imagePreviewUrl) {
+//       $imagePreview = (<img src={imagePreviewUrl} />);
+//     } else {
+//       $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+//     }
+
+//     return (
+//       <div className="previewComponent">
+//         <form onSubmit={(e)=>this.handleSubmit(e)}>
+//           <input className="fileInput" type="file" onChange={(e)=>this.handleImageChange(e)} />
+//           <button className="submitButton" type="submit" onClick={(e)=>this.handleSubmit(e)}>Upload Image</button>
+//         </form>
+//         <div className="imgPreview">
+//           {$imagePreview}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 export default ImageUpload;
