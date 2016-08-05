d17b999d1fbb58682749c6ea645e8b07febfb685
Sujin Lee <pasdesujin@gmail.com>
2016-08-05 17:37:47 -0400

    (feat) Create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let Home = () => (
+  <div>
+    <h1> Home </h1>
+    <img src="http://nordicapis.com/wp-content/uploads/20-emotion-recognition-apis-nordic-apis.png"  alt="main" />
+    <p>Speak Mirror</p>
+    <p>Friends don't let friends talk to themselves in front of a mirror. Yuan - Your friend and CEO</p>
+    <button>Start Now</button>
+  </div>
+  )
+
+export default Home;
