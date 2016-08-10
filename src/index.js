99e8fcdf396573695f47c3351b0b4b455a28baed
Sujin Lee <pasdesujin@gmail.com>
2016-08-10 09:26:57 -0400

    (chore) Refactor index.js and create sentiments reducer for Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { Router, hashHistory } from 'react-router';
+import reducers from './reducers';
 import routes from './routes';
 import './index.css';
 
 ReactDOM.render(
-  <Router history={hashHistory}>{routes}</Router>,
+  <Provider store={createStore(reducers)}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
   document.getElementById('root')
 );
