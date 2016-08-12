04cbeebcb6c0a64f7e9d380f70e3155baf9e00fe
Sujin Lee <pasdesujin@gmail.com>
2016-08-12 18:02:15 -0400

    (refactor) Refactor App into container

---
+++ src/containers/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import '../App.css';
+import Navbar from '../components/Navbar';
+
+class App extends Component {
+  render() {
+
+    const { dispatch, isAuthenticated, errorMessage } = this.props;
+
+    return (
+      <div className="App">
+        <Navbar
+        isAuthenticated={ isAuthenticated }
+        errorMessage={ errorMessage }
+        dispatch={ dispatch }
+        />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { isAuthenticated, errorMessage } = state.auth;
+
+  return {
+    isAuthenticated: isAuthenticated,
+    errorMessage: errorMessage
+  };
+}
+export default connect(mapStateToProps)(App);
