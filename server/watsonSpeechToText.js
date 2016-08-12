e5d27077cf4d9ccfde400161a60dc1e08a8acaf9
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-08-12 13:54:25 -0300

    (chore) Add dummy data from Watson SpeechToText API

---
+++ server/watsonSpeechToText.js
@@ -0,0 +1,130 @@
+{
+  "results": [
+    {
+      "word_alternatives": [
+        {
+          "start_time": 0.03,
+          "alternatives": [
+            {
+              "confidence": 0.9496,
+              "word": "the"
+            }
+          ],
+          "end_time": 0.09
+        },
+        . . .
+        {
+          "start_time": 0.86,
+          "alternatives": [
+            {
+              "confidence": 0.9471,
+              "word": "report"
+            }
+          ],
+          "end_time": 1.51
+        }
+      ],
+      "alternatives": [
+        {
+          "timestamps": [
+            [
+              "the",
+              0.03,
+              0.09
+            ],
+            . . .
+            [
+              "report",
+              0.86,
+              1.51
+            ]
+          ],
+          "confidence": 0.985,
+          "transcript": "the latest weather report "
+        }
+      ],
+      "final": true
+    }
+  ],
+  "result_index": 0
+}
+{
+  "results": [
+    {
+      "word_alternatives": [
+        {
+          "start_time": 0.14,
+          "alternatives": [
+            {
+              "confidence": 1,
+              "word": "a"
+            }
+          ],
+          "end_time": 0.29
+        },
+        {
+          "start_time": 0.29,
+          "alternatives": [
+            {
+              "confidence": 0.9999,
+              "word": "line"
+            }
+          ],
+          "end_time": 0.64
+        },
+        . . .
+        {
+          "start_time": 5.19,
+          "alternatives": [
+            {
+              "confidence": 1,
+              "word": "on"
+            }
+          ],
+          "end_time": 5.33
+        },
+        {
+          "start_time": 5.33,
+          "alternatives": [
+            {
+              "confidence": 0.9972,
+              "word": "Sunday"
+            }
+          ],
+          "end_time": 6.04
+        }
+      ],
+      "alternatives": [
+        {
+          "timestamps": [
+            [
+              "a",
+              0.14,
+              0.29
+            ],
+            [
+              "line",
+              0.29,
+              0.64
+            ],
+            . . .
+            [
+              "on",
+              5.19,
+              5.33
+            ],
+            [
+              "Sunday",
+              5.33,
+              6.04
+            ]
+          ],
+          "confidence": 0.995,
+          "transcript": "a line of severe thunderstorms with several possible tornadoes is approaching Colorado on Sunday "
+        }
+      ],
+      "final": true
+    }
+  ],
+  "result_index": 0
+}
