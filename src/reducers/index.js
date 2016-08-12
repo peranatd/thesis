08b307e3a354e47129cee771bb52a51d7eb905eb
Yuan Liu <liuyuanneu@gmail.com>
2016-08-12 17:23:31 -0400

    (refactor) Refactor reducer for auth

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import SentimentReducer from './reducer_sentiments';
 import ToneReducer from './reducer_tone';
 import SpeechToTextReducer from './reducer_speechtotext';
+import Auth from './reducer_auth';
 
 const rootReducer = combineReducers({
   sentiments: SentimentReducer,
   tone: ToneReducer,
-  speechToText: SpeechToTextReducer
+  speechToText: SpeechToTextReducer,
+  auth: Auth
 });
 
 export default rootReducer;
