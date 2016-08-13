9f1bccd66341d70596497edd394e453d2bddddc7
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-08-13 19:44:03 -0300

    (feat) Parse speech to text result from Watson API

--- src/actions/action_speechtotext.js
+++ src/actions/action_speechtotext.js
@@ -1,7 +1,9 @@
-export function SpeechToTextResponse (data,speechtotext) {
-  console.log('SPEECHTOTEXT ACTION ', speechtotext);
+export function SpeechToTextResponse (data, speechtotext) {
+  console.log('SPEECHTOTEXT ACTION ', data);
   return {
     type: 'SPEECHTOTEXT_RESPONSE',
-    payload: speechtotext.concat(data)
+    payload: speechtotext.concat([data.filter(sentence => sentence.final).reduce((a, b) => {
+      return a += b.alternatives[0].transcript;
+    }, '')])
   };
 }
