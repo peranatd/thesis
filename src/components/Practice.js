634fbfe911c84e52a09c4905226df1b9614b37b0
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-08-13 21:59:24 -0300

    (fix) Fix redux part I

--- src/components/Practice.js
+++ src/components/Practice.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TextBox from './TextBox';
 import Webcam from '../containers/Webcam';
 import Result from '../containers/Result';
+import { connect } from 'react-redux';
+import $ from 'jquery';
+
 // import ImageUpload from './imageUpload';
 // import Speech from './SpeechToText';
 
-const Practice = () => (
-  <div>
-    <h1> Practice </h1>
-    <Webcam />
-    <TextBox />
-    <Result />
-    {/*<Speech />*/}
-    {/*<ImageUpload />*/}
-  </div>
-);
+class Practice extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default Practice;
+  // handleChange () {
+  //   let text = this.state.text;
+  //   $.ajax({
+  //     url:'/api/text',
+  //     type:'POST',
+  //     data: JSON.stringify({text: text}),
+  //     contentType: 'application/json',
+  //     success: function () {
+  //       console.log('ajax post request successfully');
+  //     },
+  //     error: function () {
+  //       console.log('ajax post request failed!');
+  //     }
+  //   });
+  // }
+
+  handleTextChange (event) {
+    event.target.value
+  }
+
+  render () {
+    return (
+    <div>
+      <h1> Practice </h1>
+      <Webcam />
+      <div>{this.props.speechToText[0]}</div>
+      <TextBox speechToText={this.props.speechToText}/>
+      <Result />
+      {/*<Speech />*/}
+      {/*<ImageUpload />*/}
+    </div>
+    );
+  }
+};
+
+function mapStateToProps(state) {
+  return {
+    speechToText: state.speechToText
+  };
+}
+
+export default connect(mapStateToProps)(Practice);
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,38 +1,14 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
 
-class TextBox extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {text: ''};
-  }
-
-  handleChange () {
-    let text = this.state.text;
-    $.ajax({
-      url:'/api/text',
-      type:'POST',
-      data: JSON.stringify({text: text}),
-      contentType: 'application/json',
-      success: function () {
-        console.log('ajax post request successfully');
-      },
-      error: function () {
-        console.log('ajax post request failed!');
-      }
-    });
-  }
-
-  render () {
-    return (
-      <div >
-        <h2>TextBox</h2>
-        <p>Please upload your text file</p>
-        <textarea rows="4" placeholder="Write down your transcription here..." onChange={(e)=>{this.setState({text: e.target.value});}} />
-        <button onClick={this.handleChange.bind(this)}>Upload Text</button>
-      </div>
-    );
-  }
+const TextBox = (props) => {
+  return (
+    <div >
+      <h2>TextBox</h2>
+      <p>Please upload your text file</p>
+      <textarea rows="4" placeholder="Write down your transcription here..." value={props.speechToText[0]} />
+      <button>Upload Text</button>
+    </div>
+  );
 }
 
 export default TextBox;
