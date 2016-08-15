4e83d8a4c237e249a35f6199659fc1c80f4d37ef
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-08-15 15:11:44 -0300

    (feat) Display speech to text result

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
-const TextBox = (props) => {
-  return (
-    <div >
-      <h2>TextBox</h2>
-      <p>Please upload your text file</p>
-      <textarea rows="4" placeholder="Write down your transcription here..." value={props.speechToText[0]} />
-      <button>Upload Text</button>
-    </div>
-  );
+class TextBox extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    $('textarea').val(newProps.speechToText[0]);
+  }
+
+  handleChange() {
+    this.setState({
+      text: $('textarea').val()
+    })
+  }
+
+  handleSubmit() {
+    let text = this.state.text;
+    $.ajax({
+      url:'/api/text',
+      type:'POST',
+      data: JSON.stringify({text: text}),
+      contentType: 'application/json',
+      success: function () {
+        console.log('ajax post request successfully');
+      },
+      error: function () {
+        console.log('ajax post request failed!');
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div >
+        <h2>TextBox</h2>
+        <p>Please upload your text file</p>
+        <textarea rows="4" placeholder="Write down your transcription here..." onChange={this.handleChange.bind(this)} />
+        <button onClick={this.handleSubmit.bind(this) }>Upload Text</button>
+      </div>
+    );
+  }
 }
 
 export default TextBox;
