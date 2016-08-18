fb8f62ad70185b7b765bb717a4342aac72c16770
Peranat Dayananda <peranatdayananda@gmail.com>
2016-08-19 06:07:40 +0700

    (feat) Make speech to text update on intermediate result.
    Resolve #148

--- src/actions/action_streamingstt.js
+++ src/actions/action_streamingstt.js
@@ -1,15 +1,27 @@
 export function StreamingSttResponse (data, previous) {
   return {
     type: 'STREAMINGSTT_RESPONSE',
-    payload: [...previous, ..._streaming(data)]
+    // payload: [...previous, ..._streaming(data)]
+    payload: _moreUpdates(data, previous)
   };
 }
 
-function _streaming (data) {
-  const result = [];
+// function _streaming (data) {
+//   const result = [];
+//   if (data.results[0].final) {
+//     let sentence = data.results[0].alternatives[0].transcript;
+//     result.push([sentence[0].toUpperCase(), ...sentence.slice(1, sentence.length-1), '.', ' '].join(''));
+//   }
+//   return result;
+// }
+
+function _moreUpdates (data, previous) {
+  const result = [...previous];
+  const index = data['result_index'];
+  let sentence = data.results[0].alternatives[0].transcript;
   if (data.results[0].final) {
-    let sentence = data.results[0].alternatives[0].transcript;
-    result.push([sentence[0].toUpperCase(), ...sentence.slice(1, sentence.length-1), '.', ' '].join(''));
+    sentence = [sentence[0].toUpperCase(), ...sentence.slice(1, sentence.length-1), '.', ' '].join('');
   }
+  result[index] = sentence;
   return result;
 }
