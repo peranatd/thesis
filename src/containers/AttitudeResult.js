8825060a6962b3fb4c83ddeb4233770184ea7421
Yuan Liu <liuyuanneu@gmail.com>
2016-08-19 17:36:57 -0400

    (feat) Add spinner when waiting for response

--- src/containers/AttitudeResult.js
+++ src/containers/AttitudeResult.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loader from "react-loader";
 import { connect } from 'react-redux';
 import attitudeData from '../attitudeData';
 
@@ -8,56 +9,124 @@ class AttitudeResult extends React.Component {
     this.state = {
       temper: {src:'', info: '', displayInfo:false},
       valence: {src:'', info: '', displayInfo:false},
-      arousal: {src:'', info: '', displayInfo:false}
+      arousal: {src:'', info: '', displayInfo:false},
+      loaded: false
     };
   }
 
   showinfo(attitude){
-    console.log('what is attitude',attitude);
     this.setState({attitude:Object.assign(this.state[attitude],{displayInfo:!this.state[attitude].displayInfo})});
-    console.log('what is state temper', this.state[attitude]);
   }
 
   componentWillReceiveProps(newProps){
     let result = JSON.parse(newProps.tone)[0].result.analysisSummary.AnalysisResult;
 
     if(result.Temper.Mode === 'low'){
-      this.setState({temper:{src:attitudeData.temper.low.src, info:attitudeData.temper.low.info}});
+      this.setState({
+        temper: {
+          src:attitudeData.temper.low.src,
+          info:attitudeData.temper.low.info
+        },
+        loaded:true
+      });
     }
     if(result.Temper.Mode === 'medium'){
-      this.setState({temper:{src:attitudeData.temper.medium.src, info:attitudeData.temper.medium.info}});
+      this.setState({
+        temper: {
+          src:attitudeData.temper.medium.src,
+          info:attitudeData.temper.medium.info
+        },
+        loaded:true
+      });
     }
     if(result.Temper.Mode === 'high'){
-      this.setState({temper:{src:attitudeData.temper.high.src, info:attitudeData.temper.high.info}});
+      this.setState({
+        temper: {
+          src:attitudeData.temper.high.src,
+          info:attitudeData.temper.high.info
+        },
+        loaded:true
+      });
     }
     if(result.Valence.Mode === 'negative'){
-      this.setState({valence:{src:attitudeData.valence.negative.src, info:attitudeData.valence.negative.info}});
+      this.setState({
+        valence: {
+          src:attitudeData.valence.negative.src,
+          info:attitudeData.valence.negative.info
+        },
+        loaded:true
+      });
     }
     if(result.Valence.Mode === 'neutral'){
-      this.setState({valence:{src:attitudeData.valence.neutral.src, info:attitudeData.valence.neutral.info}});
+      this.setState({
+        valence:{
+          src:attitudeData.valence.neutral.src,
+          info:attitudeData.valence.neutral.info
+        },
+        loaded:true
+      });
     }
     if(result.Valence.Mode === 'positive'){
-      this.setState({valence:{src:attitudeData.valence.positive.src, info:attitudeData.valence.positive.info}});
+      this.setState({
+        valence:{
+          src:attitudeData.valence.positive.src,
+          info:attitudeData.valence.positive.info
+        },
+        loaded:true
+      });
     }
     if(result.Arousal.Mode === 'high'){
-      this.setState({arousal:{src:attitudeData.arousal.high.src, info:attitudeData.arousal.high.info}});
+      this.setState({
+        arousal:{
+          src:attitudeData.arousal.high.src,
+          info:attitudeData.arousal.high.info
+        },
+        loaded:true
+      });
     }
     if(result.Arousal.Mode === 'neutral'){
-      this.setState({arousal:{src:attitudeData.arousal.neutral.src, info:attitudeData.arousal.neutral.info}});
+      this.setState({
+        arousal:{
+          src:attitudeData.arousal.neutral.src,
+          info:attitudeData.arousal.neutral.info
+        },
+        loaded:true
+      });
     }
     if(result.Arousal.Mode === 'low'){
-      this.setState({arousal:{src:attitudeData.arousal.low.src, info:attitudeData.arousal.low.info}});
+      this.setState({
+        arousal:{
+          src:attitudeData.arousal.low.src,
+          info:attitudeData.arousal.low.info
+        },
+        loaded:true
+      });
     }
   }
 
   render() {
     return (
       <div>
-        <div><span>Temper:</span><img className="icon" src={this.state.temper.src} onClick={this.showinfo.bind(this,'temper')}/></div>
+        <div>
+          <span>Temper:</span>
+          <Loader loaded={this.state.loaded}>
+            <img className="icon" src={this.state.temper.src} onClick={this.showinfo.bind(this,'temper')}/>
+          </Loader>
+        </div>
         {this.state.temper.displayInfo ? <div>{this.state.temper.info}</div> : null}
-        <div><span>Valence:</span><img className="icon" src={this.state.valence.src} onClick={this.showinfo.bind(this,'valence')} /></div>
+        <div>
+          <span>Valence:</span>
+          <Loader loaded={this.state.loaded}>
+            <img className="icon" src={this.state.valence.src} onClick={this.showinfo.bind(this,'valence')} />
+          </Loader>
+        </div>
         {this.state.valence.displayInfo ? <div>{this.state.valence.info}</div> : null}
-        <div><span>Arousal:</span><img className="icon" src={this.state.arousal.src} onClick={this.showinfo.bind(this,'arousal')} /></div>
+        <div>
+          <span>Arousal:</span>
+          <Loader loaded={this.state.loaded}>
+            <img className="icon" src={this.state.arousal.src} onClick={this.showinfo.bind(this,'arousal')} />
+          </Loader>
+        </div>
         {this.state.arousal.displayInfo ? <div>{this.state.arousal.info}</div> : null}
       </div>
     );
