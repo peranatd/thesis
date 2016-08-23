3b97eae8a644538b19bca4f18355961095ed705e
Sujin Lee <pasdesujin@gmail.com>
2016-08-23 00:37:24 -0400

    (feat) Add feature descriptions on home page with style

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,31 @@ import { Link } from 'react-router';
 import Slide from './Slide';
 
 const Home = () => (
-  <div>
+  <div className="wrapper">
     <Slide />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-7">
+          <h2 className="featurette-heading"><span className="glyphicon glyphicon-cloud-download"></span>Powerful APIs</h2>
+          <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
+          </p>
+        </div>
+        <div className="col-md-5">
+          <img src="https://placekitten.com/500/300" className="img-responsive" />
+        </div>
+      </div>
+      <hr className="featurette-divider" />
+      <div className="row">
+        <div className="col-md-5">
+          <img src="https://placekitten.com/500/300" className="img-responsive" />
+        </div>
+        <div className="col-md-7">
+          <h2 className="featurette-heading"><span className="glyphicon glyphicon-stats"></span>Live D3 Charts</h2>
+          <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
   );
 
