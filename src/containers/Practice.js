cf2fddd792a09d1bfbc24bb6e7d83c1f18c1681f
Sujin Lee <pasdesujin@gmail.com>
2016-08-24 14:07:36 -0400

    (feat) Add instruction and style on practice page

--- src/containers/Practice.js
+++ src/containers/Practice.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import { msEmotionReset }  from '../actions/action_msEmotion';
 import TextBox from '../components/TextBox';
 import Webcam from './Webcam';
 import Chart from './Chart';
@@ -20,8 +18,7 @@ class Practice extends Component {
     };
   }
 
-  componentWillMount() {
-    this.props.msEmotionReset(null, this.props.msEmotion);
+  componentDidMount() {
     this.props.socket.emit('sessionStart', {
       sessionTimestamp: this.state.sessionTimestamp,
       username: this.context.user.username
@@ -36,22 +33,42 @@ class Practice extends Component {
     return (
     <div className="container">
       <h1> Practice </h1>
-      <div className="row">
+      <div className="row practice">
         <Webcam
           sessionTimestamp={this.state.sessionTimestamp}
           user={this.context.user}
         />
         <TextBox
+          speechToText={this.props.speechToText}
           sessionTimestamp={this.state.sessionTimestamp}
           user={this.context.user}
         />
       </div>
-      <div className="row">
-        <Chart emotion={this.props.msEmotion}/>
-      </div>
-      <div className="row">
-        <Cloud trans={this.props.transcription}/>
-      </div>
+      {!this.props.msEmotion.length && !this.props.transcription.length ?
+        <div className="row">
+          <h3><span className="glyphicon glyphicon-exclamation-sign"></span> How to practice</h3>
+          <p>1. Press start button to record your face and voice</p>
+          <p>2. Press stop button to stop recording</p>
+          <p>3. Press submit button to see your analysis result</p>
+        </div>:
+        <div>
+          <div className="row">
+            <h3 className="resultSection">Sentiment Result</h3>
+            <Chart emotion={this.props.msEmotion}/>
+            <p>{"The emotions detected are happiness, sadness, surprise, anger, fear, contempt, disgust or neutral."}</p>
+          </div>
+          <hr className="featurette-divider" />
+          <div className="row">
+            <div className="col-md-9">
+              <h3 className="resultSection">Speech Word Cloud</h3>
+              <Cloud trans={this.props.transcription}/>
+            </div>
+            <div className="col-md-3">
+              <p>{"The clouds give greater prominence to words that appear more frequently in the source text"}</p>
+            </div>
+          </div>
+        </div>
+      }
     </div>
     );
   }
@@ -60,15 +77,10 @@ class Practice extends Component {
 function mapStateToProps(state) {
   return {
     socket: state.socket,
+    speechToText: state.speechToText,
     msEmotion: state.msEmotion,
     transcription: state.transcription
   };
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({
-    msEmotionReset: msEmotionReset
-  }, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Practice);
+export default connect(mapStateToProps)(Practice);
