a2b19251f1a615ad7cd3a2c3947402501b244b56
Yuan Liu <liuyuanneu@gmail.com>
2016-08-24 18:39:27 -0400

    (style) Add style for profile page

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -61,14 +61,15 @@ class ProfilePage extends Component {
         <div className="container">
           <div className="row">
             <div className="col-xs-12">
-              <h3>My Profile</h3>
+              <h3 className="resultSection">My Profile</h3>
             </div>
           </div>
+
           <div className="row">
-            <div className="col-xs-6">
-              <p>Please select sessions</p>
+            <div className="col-md-4">
+              <label className="selecting">Please select sessions</label>
             </div>
-            <div className="col-xs-6">
+            <div className="col-md-8">
               <select className="form-control" onChange={this.handleChange.bind(this)}>
                 <option value='null'>Please select sessions</option>
                 {this.state.sessions.map((session) => {
@@ -78,46 +79,79 @@ class ProfilePage extends Component {
               </select>
             </div>
           </div>
+
           { this.state.currentSession ? (
           <div>
-            <div className="row">
-              <h3>Sentiment Result</h3>
-              {
-                this.state.result.msEmotion ?
-                <Chart emotion={this.state.result.msEmotion}/> :
-                <p>{"Sorry you don't have any sentiment result yet. Please go to practice page practicing first. Thank you!"}</p>
-              }
-            </div>
-            <div className="row">
-              <h3>Speech Word Cloud</h3>
-              {
-                this.state.result.watson ?
-                <Cloud trans={[this.state.result.watson.transcript]}/> :
-                <p>{"Sorry you don't have any word cloud yet. Please go to practice page adding text in the textbox first. Thank you!"}</p>
-              }
-            </div>
-            <div className="row">
-              <h3>Speech Content Sentiment Result</h3>
-              {
-                this.state.result.watson ?
-                <Radar watson={this.state.result.watson}/> :
-                <p>{"Sorry you don't have any sentiment result yet. Please go to practice page recording first. Thank you!"}</p>
-              }
-            </div>
-            <div className="row">
-              <h3>Speech Tone Sentiment Result</h3>
-              {
-                this.state.result.bv ? (
+            <div className="Result">
+              <h3 className="resultSection">Sentiment Result</h3>
+              <div className="row">
                 <div>
-                  <Tone_Cloud tone={[this.state.result.bv]}/>
-                  <AttitudeResult tone={[this.state.result.bv]}/>
-                  <ToneSummary tone={[this.state.result.bv]}/>
+                  {
+                    this.state.result.msEmotion ?
+                    <Chart emotion={this.state.result.msEmotion}/> :
+                    <p>{"Sorry you don't have any sentiment result yet. Please go to practice page practicing first. Thank you!"}</p>
+                  }
                 </div>
-                ): <p>{"Sorry you don't have any sentiment result yet. Please go to practice page recording first. Thank you!"}</p>
-              }
+                <div>
+                  {
+                    this.state.result.msEmotion ? <p>{"The emotions detected are happiness, sadness, surprise, anger, fear, contempt, disgust or neutral."}</p> : null
+                  }
+                </div>
+              </div>
+            </div>
+            <hr className="featurette-divider"/>
+            <div className="Result">
+              <h3 className="resultSection">Speech Word Cloud</h3>
+              <div className="row">
+                <div >
+                  {
+                    this.state.result.watson ?
+                    <Cloud trans={[this.state.result.watson.transcript]}/> :
+                    <p>{"Sorry you don't have any word cloud yet. Please go to practice page adding text in the textbox first. Thank you!"}</p>
+                  }
+                </div>
+                <div >
+                  {
+                    this.state.result.watson ? <p>{"The clouds give greater prominence to words that appear more frequently in the source text"}</p> : null
+                  }
+                </div>
+              </div>
+            </div>
+            <hr className="featurette-divider"/>
+            <div className="Result">
+              <h3 className="resultSection">Speech Content Sentiment Result</h3>
+                {
+                  this.state.result.watson ?
+                  <Radar watson={this.state.result.watson}/> :
+                  <p>{"Sorry you don't have any sentiment result yet. Please go to practice page recording first. Thank you!"}</p>
+                }
+            </div>
+            <hr className="featurette-divider"/>
+            <div className="Result">
+              <h3 className="resultSection">Speech Tone Sentiment Result</h3>
+              <div className="row">
+                {
+                  this.state.result.bv ? (
+                  <div>
+                    <div className="col-md-3">
+                      <h4 className="toneTitle">Tone Cloud</h4>
+                      <Tone_Cloud tone={[this.state.result.bv]}/>
+                    </div>
+                    <div className="col-md-6">
+                      <h4 className="toneTitle">Attitude Result</h4>
+                      <AttitudeResult tone={[this.state.result.bv]}/>
+                    </div>
+                    <div className="col-md-3">
+                      <h4 className="toneTitle">Tone Summary</h4>
+                      <ToneSummary tone={[this.state.result.bv]}/>
+                    </div>
+                  </div>
+                  ): <p>{"Sorry you don't have any sentiment result yet. Please go to practice page recording first. Thank you!"}</p>
+                }
+              </div>
             </div>
           </div>) :
-          <div className="row">
+          <div className="row Result">
             <p><span className="glyphicon glyphicon-warning-sign"></span>You have no history</p>
           </div>
           }
