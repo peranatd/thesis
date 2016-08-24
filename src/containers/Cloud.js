5d495cd091f7f3231a8493f91ec0ff298acea59a
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-08-24 02:43:10 -0300

    (refactor) Refactor Cloud component

--- src/containers/Cloud.js
+++ src/containers/Cloud.js
@@ -9,122 +9,74 @@ class Cloud extends Component {
   constructor(props){
     super(props);
   }
-  componentDidMount(){
-    if(this.props.trans.length){
-      const options = {
-        language:"english",
-        remove_digits: true,
-        return_changed_case:true,
-        remove_duplicates: false
-      };
-      let max = 1;
-      let wordList = keyword.extract(this.props.trans[0], options).reduce((memo, item)=>{
-        item = item.toLowerCase();
-        if (item in memo) {
-          memo[item] += 1;
-          if (memo[item] > max) { max = memo[item]; }
-        } else {
-          memo[item] = 1;
-        }
-        return memo;
-      },{});
 
-      let wordArray = Object.keys(wordList).map((word)=>{
-        return {text: word, size: wordList[word]};
-      });
-
-      let fill = scaleOrdinal(schemeCategory20);
-
-      let layout = cloud()
-          .size([500, 500])
-          .words(wordArray)
-          .spiral('archimedean')
-          .font('Impact')
-          .fontSize(function(d) { return 10 + 60*Math.pow(d.size/max, 1); })
-          .on('end', draw);
-
-      layout.start();
-
-      function draw (words) {
-        d3.select('.wordCloud').remove();
-
-        d3.select('#cloud')
-            .attr('width', layout.size()[0])
-            .attr('height', layout.size()[1])
-          .append('g')
-            .attr('transform', 'translate(' + layout.size()[0] / 2 + ',' + layout.size()[1] / 2 + ')')
-            .attr('class', 'wordCloud')
-          .selectAll('text')
-            .data(words)
-          .enter().append('text')
-            .style('font-size', function(d) { return d.size + 'px'; })
-            .style('font-family', 'Impact')
-            .style('fill', function(d, i) { return fill(i); })
-            .attr('text-anchor', 'middle')
-            .attr('transform', function(d) {
-              return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
-            })
-            .text(function(d) { return d.text; });
+  formatData(props) {
+    const options = {
+      language:"english",
+      remove_digits: true,
+      return_changed_case:true,
+      remove_duplicates: false
+    };
+    let max = 1;
+    let wordList = keyword.extract(props.trans[0], options).reduce((memo, item)=>{
+      item = item.toLowerCase();
+      if (item in memo) {
+        memo[item] += 1;
+        if (memo[item] > max) { max = memo[item]; }
+      } else {
+        memo[item] = 1;
       }
+      return memo;
+    },{});
+
+    let wordArray = Object.keys(wordList).map((word)=>{
+      return {text: word, size: wordList[word]};
+    });
+
+    let fill = scaleOrdinal(schemeCategory20);
+
+    let layout = cloud()
+        .size([500, 500])
+        .words(wordArray)
+        .spiral('archimedean')
+        .font('Impact')
+        .fontSize(function(d) { return 10 + 60*Math.pow(d.size/max, 1); })
+        .on('end', draw);
+
+    layout.start();
+
+    function draw (words) {
+      d3.select('.wordCloud').remove();
+
+      d3.select('#cloud')
+          .attr('width', layout.size()[0])
+          .attr('height', layout.size()[1])
+        .append('g')
+          .attr('transform', 'translate(' + layout.size()[0] / 2 + ',' + layout.size()[1] / 2 + ')')
+          .attr('class', 'wordCloud')
+        .selectAll('text')
+          .data(words)
+        .enter().append('text')
+          .style('font-size', function(d) { return d.size + 'px'; })
+          .style('font-family', 'Impact')
+          .style('fill', function(d, i) { return fill(i); })
+          .attr('text-anchor', 'middle')
+          .attr('transform', function(d) {
+            return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
+          })
+          .text(function(d) { return d.text; });
     }
   }
+
+  componentDidMount(){
+    if(this.props.trans.length){
+      this.formatData(this.props);
+    }
+  }
+
   componentWillReceiveProps (newProps) {
-    if (newProps.transcription.length) {
-      const options = {
-        language:"english",
-        remove_digits: true,
-        return_changed_case:true,
-        remove_duplicates: false
-      };
-      let max = 1;
-      let wordList = keyword.extract(newProps.transcription[0], options).reduce((memo, item)=>{
-        item = item.toLowerCase();
-        if (item in memo) {
-          memo[item] += 1;
-          if (memo[item] > max) { max = memo[item]; }
-        } else {
-          memo[item] = 1;
-        }
-        return memo;
-      },{});
-
-      let wordArray = Object.keys(wordList).map((word)=>{
-        return {text: word, size: wordList[word]};
-      });
-
-      let fill = scaleOrdinal(schemeCategory20);
-
-      let layout = cloud()
-          .size([500, 500])
-          .words(wordArray)
-          .spiral('archimedean')
-          .font('Impact')
-          .fontSize(function(d) { return 10 + 60*Math.pow(d.size/max, 1); })
-          .on('end', draw);
-
-      layout.start();
-
-      function draw (words) {
-        d3.select('.wordCloud').remove();
-
-        d3.select('#cloud')
-            .attr('width', layout.size()[0])
-            .attr('height', layout.size()[1])
-          .append('g')
-            .attr('transform', 'translate(' + layout.size()[0] / 2 + ',' + layout.size()[1] / 2 + ')')
-            .attr('class', 'wordCloud')
-          .selectAll('text')
-            .data(words)
-          .enter().append('text')
-            .style('font-size', function(d) { return d.size + 'px'; })
-            .style('font-family', 'Impact')
-            .style('fill', function(d, i) { return fill(i); })
-            .attr('text-anchor', 'middle')
-            .attr('transform', function(d) {
-              return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
-            })
-            .text(function(d) { return d.text; });
-      }
+    if (newProps.trans.length) {
+      this.formatData(newProps);
     }
   }
 
@@ -138,10 +90,4 @@ class Cloud extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    transcription: state.transcription
-  };
-}
-
-export default connect(mapStateToProps)(Cloud);
+export default Cloud;
