e05daed9213f495da94893078cbe52c8c47bb32f
Yuan Liu <liuyuanneu@gmail.com>
2016-08-24 22:19:44 -0400

    (chore) Delete result page

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,6 @@ import RegisterPage from './components/RegisterPage';
 import ResetPasswordPage from './components/ResetPasswordPage';
 import ProfilePage from './containers/ProfilePage';
 import Team from './components/Team';
-import Result from './containers/Result';
 
 export default (
   <Route path="/" component={App}>
@@ -24,9 +23,6 @@ export default (
     </AuthenticatedRoute>
     <AuthenticatedRoute>
       <Route path="/practice" component={Practice} />
-    </AuthenticatedRoute>
-    <AuthenticatedRoute>
-      <Route path="/result" component={Result} />
     </AuthenticatedRoute>
     <Route path="/team" component={Team} />
   </Route>
