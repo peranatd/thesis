d14542dd9c39d3ca3a6c89a3b2a58360530d57b7
Sujin Lee <pasdesujin@gmail.com>
2016-08-26 00:55:22 -0400

    (chore) Add linkedin links and team repository link with style

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,60 +1,48 @@
 import React from 'react';
 import github from '../logos/github.svg';
+import linkedin from '../logos/linkedin.svg';
 
 const Team = () => (
   <div className="container">
     <h1>Team Little Big Planet</h1>
-    <div className="media">
-      <div className="media-left media-middle">
-        <img className="media-object img-circle" src="https://s3.postimg.org/4b5s5i21v/10687021_1513080458908601_2030217230142341681_n.jpg" alt="Felipe Matsumoto" />
-      </div>
-      <div className="media-body text-left">
-        <h4 className="media-heading">Felipe Matsumoto</h4>
-        <h5 className="media-heading">CFO</h5>
+    <div className="row main">
+      <div className="col-sm-3">
+        <img className="img-circle" src="https://s3.postimg.org/4b5s5i21v/10687021_1513080458908601_2030217230142341681_n.jpg" alt="Felipe Matsumoto" />
+        <h4>Felipe Matsumoto</h4>
+        <h5>CFO</h5>
         <a href="https://github.com/ftmatsumoto"><img className="github" src={github} /></a>
-        <p>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-        </p>
+        <a href="https://br.linkedin.com/in/felipematsumoto"><img className="linkedin" src={linkedin} /></a>
       </div>
-    </div>
-    <div className="media">
-      <div className="media-left media-middle">
-        <img className="media-object img-circle" src="https://s4.postimg.org/ouzufy87h/48467461251_25de3636c979d7bdcaa1_512.jpg" alt="Peranat Dayananda" />
-      </div>
-      <div className="media-body text-left">
-        <h4 className="media-heading">Peranat Dayananda</h4>
-        <h5 className="media-heading">Product Owner</h5>
+
+      <div className="col-sm-3">
+        <img className="img-circle" src="https://s4.postimg.org/ouzufy87h/48467461251_25de3636c979d7bdcaa1_512.jpg" alt="Peranat Dayananda" />
+        <h4>Peranat Dayananda</h4>
+        <h5>Product Owner</h5>
         <a href="https://github.com/peranatd"><img className="github" src={github} /></a>
-        <p>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-        </p>
+        <a href="https://www.linkedin.com/in/peranatdayananda"><img className="linkedin" src={linkedin} /></a>
       </div>
-    </div>
-    <div className="media">
-      <div className="media-left media-middle">
-        <img className="media-object img-circle" src="https://s3.postimg.org/59w0ymt4z/image.jpg" alt="Sujin Lee" />
-      </div>
-      <div className="media-body text-left">
-        <h4 className="media-heading">Sujin Lee</h4>
-        <h5 className="media-heading">Scrum Master</h5>
+
+      <div className="col-sm-3">
+        <img className="img-circle" src="https://s3.postimg.org/59w0ymt4z/image.jpg" alt="Sujin Lee" />
+        <h4>Sujin Lee</h4>
+        <h5>Scrum Master</h5>
         <a href="https://github.com/pasdesujin"><img className="github" src={github} /></a>
-        <p>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-        </p>
+        <a href="https://www.linkedin.com/in/pasdesujin"><img className="linkedin" src={linkedin} /></a>
+      </div>
+
+      <div className="col-sm-3">
+        <img className="img-circle" src="https://s4.postimg.org/md6bk5jgd/2201231.jpg" alt="Yuan Liu" />
+        <h4>Yuan Liu</h4>
+        <h5>CEO</h5>
+        <a href="https://github.com/liuyuanneu"><img className="github" src={github} /></a>
+        <a href="https://www.linkedin.com/in/liuyuanneu"><img className="linkedin" src={linkedin} /></a>
       </div>
     </div>
-    <div className="media">
-      <div className="media-left media-middle">
-        <img className="media-object img-circle" src="https://s4.postimg.org/md6bk5jgd/2201231.jpg" alt="Yuan Liu" />
-      </div>
-      <div className="media-body text-left">
-        <h4 className="media-heading">Yuan Liu</h4>
-        <h5 className="media-heading">CEO</h5>
-        <a href="https://github.com/liuyuanneu"><img className="github" src={github} /></a>
-        <p>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-        </p>
-      </div>
+    <hr className="featurette-divider" />
+    <div className="row">
+      <h4>Do you want to see our team repository?</h4>
+      <a href="https://github.com/hrr17-littlebigplanet/thesis"><span className="glyphicon glyphicon-globe
+"> </span></a>
     </div>
   </div>
 );
